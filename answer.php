4f0675309163dca248b278fa5acdb432cc6b6ad3
shannon7wallace <shannon_c_wallace@yahoo.com>
2012-10-19 15:23:37 -0300

    Elijah's update

--- answer.php
+++ answer.php
@@ -1,19 +1,47 @@
 <?php
 
-$apiKey = "e6ebed29-a73a-4af7-9344-bea87e897a30";
-$consumerSecret = "0a4852eccf754a06924d3d3a3239c9cf";
-$redirectURI = "http://localhost:8080/Oauth2/answer.php";
+header('Content-type: text/html');
+
+// Start the Session and create a DataStore Object for storing this user.
+session_start ();
+require_once "ConstantContact.php";
+$Datastore = new CTCTDataStore ();
+
+//Put your API Key, consumerSecret and RedirectURI here.
+$apiKey = "";
+$consumerSecret = "";
+$redirectURI = "";
 
 $code = $_REQUEST['code'];
-$token = $_REQUEST['access_token'];
+$username = $_REQUEST['username'];
 
-echo "<br><br><a href='https://oauth2.constantcontact.com/oauth2/oauth/token?grant_type=authorization_code&client_id=$apiKey&client_secret=$consumerSecret&code=$code&
-redirect_uri=$redirectURI'>Click Here!</a>";
+//We will use PHP cURL to make a request to Constant Contact to get the Access Token.
 
-if ($_REQUEST['code'] != null)
-{
-	echo "<br><br><br><p>Click the link below if you have already generated an access token and added it to your files.</p><br>";
-	echo "<a href='https://api.constantcontact.com/v2/contact/?access_token=$token'>Contact</a>";
+$rqurl = "https://oauth2.constantcontact.com/oauth2/oauth/token?grant_type=authorization_code&client_id=$apiKey&client_secret=$consumerSecret&code=$code&redirect_uri=$redirectURI";
+$rq = curl_init();
+
+curl_setopt($rq, CURLOPT_URL, $rqurl);
+curl_setopt($rq, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($rq, CURLOPT_BINARYTRANSFER, 1);
+curl_setopt($rq, CURLOPT_HEADER, 0);
+curl_setopt($rq, CURLOPT_SSL_VERIFYPEER, 0);
+
+if (!$result = curl_exec($rq)) {
+	//If there is an error, dump it so we can see it.
+	echo curl_error($rq);
+} else {
+	echo "<pre>Access Token Information:\n";
+	print_r($result);
+	$obj = json_decode($result); // The Result of this request is returned as JSON, so we need to parse it.
+	//Create the User Ojbect.
+	$user['username'] = $username;
+	$user['access_token'] = $obj->access_token;
+	//Use the DataStore to store the User in the PHP Session.
+	$Datastore->addUser($user);
+	//Create a link back to index.php where we can now use the Session Data.
+	echo "\n<a href='http://127.0.0.1/oauth/index.php'>Click Here to Continue</a></pre>";
 }
+
+curl_close($rq);
 
 ?>
